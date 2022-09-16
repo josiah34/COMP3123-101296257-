43334f2fdb2558e867c6128574c5d63d28d202f0
josiah34 <josiah_g@live.com>
2022-09-16 18:19:23 -0400

    Finished lab 2 exercise

--- Lab2/lab2.js
+++ Lab2/lab2.js
@@ -1,23 +1,23 @@
 // Exercise 1
 const greeting = "Hello "
 myArray = ["John", "Stacey", "Michael"]
-myArray.forEach(item => console.log(greeting+item))
+myArray.forEach(item => console.log(greeting + item))
 
 
 // Exercise 2 
 const capitalize = ([first, ...rest], lowerRest = false) =>
   first.toUpperCase() + (lowerRest ? rest.join('').toLowerCase() : rest.join(''));
 
-console.log(capitalize('fooBar')); 
+console.log(capitalize('fooBar'));
 console.log(capitalize('fooBar', true));
 // Exercise 3
 colors = ['red', 'green', 'blue']
 const capitalizedColors = colors.map(x => capitalize(x))
 console.log(capitalizedColors)
 // Exercise 4
-var values = [1, 60, 34, 30,20, 5]
+var values = [1, 60, 34, 30, 20, 5]
 
-let filtered = values.filter(value => value < 20  )
+let filtered = values.filter(value => value < 20)
 console.log(filtered)
 
 // Exercise 5
@@ -29,3 +29,28 @@ const calculateProduct = (returnValue, currentValue) => returnValue * currentVal
 console.log(array.reduce(calculateSum))
 console.log(array.reduce(calculateProduct))
 
+class Car {
+  constructor(year, model) {
+    this.year = year
+    this.model = model
+
+  }
+  details() {
+    return "Make: " + this.model + "\n" + "Year: " + this.year
+  }
+
+}
+
+class Sedan extends Car{
+  constructor(year, model, balance) {
+    super(year, model)
+    this.balance = balance}
+  info(){
+    return "Make: " + this.model + "\n" + "Year: " + this.year+"\n"+"Balance: "+this.balance
+  }
+}
+
+let civic = new Car(1996, "Civic")
+console.log(civic.details())
+let m3 = new Sedan(1996, "M3", 3000)
+console.log(m3.info())
